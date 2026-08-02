67b96c06548e6a15affdbd6ef1d600eee8074a9c
nhatlinh6605 <linhvn6605@ut.edu.vn>
2026-08-02 16:11:53 +0700

    Thêm đoạn test tạm trong Program.cs để kiểm tra FileStorageService.SaveFileAsync
    Đoạn test này sẽ được thay thế khi tích hợp với Server TCP listener thật (phần của Anh Kiệt).

--- Code/Server/Program.cs
+++ Code/Server/Program.cs
@@ -1,1 +1,24 @@
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace UDM10.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // ===== TEST TẠM - xóa sau khi test xong =====
+            var testLogger = new ServerLogger("Extra/TestLogs/server.log");
+            var testStorage = new FileStorageService("Extra/TestData/Uploads", testLogger);
+
+            byte[] fakeData = System.Text.Encoding.UTF8.GetBytes("Xin chao, day la file test!");
+            using (var testStream = new MemoryStream(fakeData))
+            {
+                string savedPath = await testStorage.SaveFileAsync("hello.txt", fakeData.Length, testStream);
+                Console.WriteLine($"Đã lưu file tại: {savedPath}");
+            }
+            // ===== HẾT TEST TẠM =====
+        }
+    }
+}
