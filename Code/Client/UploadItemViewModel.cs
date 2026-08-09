8484f56e6b467662dd19241936c75bc365630c2d
Le Van Nhut <nhut20305@gmail.com>
2026-08-09 16:59:42 +0700

    feat: Add progress/status tracking to UploadItemViewModel

--- Code/Client/UploadItemViewModel.cs
+++ Code/Client/UploadItemViewModel.cs
@@ -6,33 +6,42 @@ namespace UDM10.Client
 {
     public class UploadItemViewModel : INotifyPropertyChanged
     {
-        private string _statusMessage = "Chưa upload";
-
         public string FileName { get; }
         public string FilePath { get; }
         public long FileSizeBytes { get; }
         public string FileSizeText => FormatSize(FileSizeBytes);
 
-        public string StatusMessage
+        private double _percentComplete;
+        public double PercentComplete
         {
-            get => _statusMessage;
-            set
-            {
-                if (_statusMessage == value)
-                {
-                    return;
-                }
-
-                _statusMessage = value;
-                OnPropertyChanged();
-            }
+            get => _percentComplete;
+            set { _percentComplete = value; OnPropertyChanged(); }
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        private double _speedKBps;
+        public double SpeedKBps
+        {
+            get => _speedKBps;
+            set { _speedKBps = value; OnPropertyChanged(); }
+        }
+
+        private UploadItemStatus _status = UploadItemStatus.Waiting;
+        public UploadItemStatus Status
+        {
+            get => _status;
+            set { _status = value; OnPropertyChanged(); }
+        }
+
+        private string _message = "";
+        public string Message
+        {
+            get => _message;
+            set { _message = value; OnPropertyChanged(); }
+        }
 
         public UploadItemViewModel(string filePath)
         {
-            FileInfo info = new(filePath);
+            var info = new FileInfo(filePath);
             FilePath = filePath;
             FileName = info.Name;
             FileSizeBytes = info.Length;
@@ -40,22 +49,13 @@ namespace UDM10.Client
 
         private static string FormatSize(long bytes)
         {
-            if (bytes >= 1024 * 1024)
-            {
-                return $"{bytes / (1024.0 * 1024):F1} MB";
-            }
-
-            if (bytes >= 1024)
-            {
-                return $"{bytes / 1024.0:F1} KB";
-            }
-
+            if (bytes >= 1024 * 1024) return $"{bytes / (1024.0 * 1024):F1} MB";
+            if (bytes >= 1024) return $"{bytes / 1024.0:F1} KB";
             return $"{bytes} B";
         }
 
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
