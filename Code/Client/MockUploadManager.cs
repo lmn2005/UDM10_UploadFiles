4f5b828f79cc06c2a9d15027700e0ada1e0e37f1
Le Van Nhut <nhut20305@gmail.com>
2026-08-09 16:59:19 +0700

    feat: Add IUploadManager interface and mock implementation

---
+++ Code/Client/MockUploadManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UDM10.Client
+{
+    public class MockUploadManager : IUploadManager
+    {
+        public void EnqueueFile(string filePath, IProgress<UploadProgress> progress)
+        {
+            _ = Task.Run(async () =>
+            {
+                for (int i = 0; i <= 100; i += 10)
+                {
+                    await Task.Delay(300);
+                    progress.Report(new UploadProgress
+                    {
+                        PercentComplete = i,
+                        SpeedKBps = 512,
+                        Status = i < 100 ? UploadItemStatus.Uploading : UploadItemStatus.Completed,
+                        Message = i < 100 ? "Đang tải..." : "Hoàn tất"
+                    });
+                }
+            });
+        }
+    }
+}
