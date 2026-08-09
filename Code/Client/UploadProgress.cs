e2b1374916f3df562d615d852774be6eb4375ca7
Le Van Nhut <nhut20305@gmail.com>
2026-08-09 16:57:58 +0700

    feat: Add UploadProgress model and UploadItemStatus enum

---
+++ Code/Client/UploadProgress.cs
@@ -0,0 +1,12 @@
+﻿namespace UDM10.Client
+{
+    public enum UploadItemStatus { Waiting, Uploading, Completed, Error }
+
+    public class UploadProgress
+    {
+        public double PercentComplete { get; set; }
+        public double SpeedKBps { get; set; }
+        public UploadItemStatus Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
